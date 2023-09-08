0819059180b6c912533e16c309545abe775b47ee
Israel <isltimi1000@gmail.com>
2023-09-08 23:20:47 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+
+function getDayOfTheWeek() {
+    const date = new Date();
+    daysoftheweek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturdau", "Sunday"]
+    today = date.getDay();
+    return (`Current Day of the week is: ${daysoftheweek[today - 1]}`);
+}
+
+const displayDay = document.getElementById("dayofweek");
+
+displayDay.innerHTML = getDayOfTheWeek();
+
+
+
+function getTimeInUTC() {
+    const currentTimeMillis = new Date().getTime();
+    return (`Current UTC Time in milliseconds: ${currentTimeMillis}`);
+}
+
+const timeElement = document.getElementById("utctime");
+
+timeElement.innerHTML = getTimeInUTC();
